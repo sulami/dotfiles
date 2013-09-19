81c9262b17360d1a55257f2f4eb679bc3b92c65f
Rob Schroer <sulamiification@gmail.com>
2013-09-19 15:22:52 +0200

    irssi: fixed xchatnickcolor.pl for proper alignment

--- .irssi/scripts/autorun/xchatnickcolor.pl
+++ .irssi/scripts/autorun/xchatnickcolor.pl
@@ -107,7 +107,7 @@ sub sig_public {
 
   $color = "0".$color if ($color < 10);
   $server->command('/^format pubmsg %b<%w$2'.chr(3).$color.'$[-11]0%b>
-%K|%n $1');
+%K |%n $1');
  # $server->command('/^format action_public {pubaction
  # '.chr(3).$color.'$0}$1');
 }
