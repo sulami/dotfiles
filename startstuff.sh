42230c22768fd98dd4a237a2e2da16f247c6181a
Robin Schroer <sulamiification@gmail.com>
2014-08-01 18:52:09 +0200

    remove startstuff script

--- startstuff.sh
+++
@@ -1,8 +0,0 @@
-sudo ifup br0
-sudo ifup em1
-mount ~/mnt/data
-cd ~/mnt/data/Bilder/Wallpapers/Space-HQ-Set/
-sh rotate_wallpaper.sh > /dev/null &
-cd ~/build/Disk-Indicator/
-sudo ./disk_indicator &
-
