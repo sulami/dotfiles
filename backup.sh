714f8b73ab3e5079743935fd8c17947e4460e1f9
Robin Schroer <sulamiification@gmail.com>
2014-10-22 01:38:44 +0200

    add backup script

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PARENT="/home/sulami/"
+
+DIRS=("Documents"
+      "git"
+      "Music"
+      "Pictures"
+      "SteamLibrary")
+
+TARGET="/home/sulami/mnt/data/"
+
+for DIR in ${DIRS[*]}
+do
+    echo "$PARENT$DIR -> $TARGET$DIR"
+    echo "[$(date --rfc-3339=date)] $PARENT$DIR" >> $TARGET"backup.log"
+    rsync -aPq --delete $PARENT$DIR $TARGET | tee -a $TARGET"backup.log"
+done
+
