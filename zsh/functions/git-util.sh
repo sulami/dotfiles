9ccf4017d8ec97628726e4ab6853c9fe7798932a
Robin Schroer <sulamiification@gmail.com>
2014-10-23 22:15:39 +0200

    zsh: fix git_add_remote to work with github

--- zsh/functions/git-util.sh
+++ zsh/functions/git-util.sh
@@ -1,6 +1,11 @@
+#!/bin/zsh
+
 git_add_remote()
 {
-    mkdir -p $2 && git init --bare $2
+    if [[ ! "$2" =~ "^(http|https|git)://.*" ]]
+    then
+        mkdir -p $2 && git init --bare $2
+    fi
     git remote add $1 $2
 }
 
