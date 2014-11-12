ea2bbada5969ae4795048f8b062b807e67057330
Robin Schroer <sulami@peerwire.org>
2014-11-12 12:39:49 +0100

    fix backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-PARENT="/home/sulami/"
+PARENT="/home/sulami"
 
 DIRS=("Documents"
       "git"
       "Music"
       "Pictures")
 
-TARGET="/home/sulami/mnt/raspi/"
+TARGET="/home/sulami/mnt/raspi"
 
 mount $TARGET
 
 for DIR in ${DIRS[*]}
 do
-    echo "$PARENT$DIR -> $TARGET$DIR"
-    echo "[$(date --rfc-3339=date)] $PARENT$DIR" >> $TARGET"backup.log"
-    rsync -aPq --delete $PARENT$DIR $TARGET | tee -a $TARGET"backup.log"
+    echo "$PARENT/$DIR -> $TARGET/$DIR"
+    echo "[$(date --rfc-3339=date)] $PARENT/$DIR" >> "$TARGET/backup.log"
+    rsync -aPq --delete $PARENT/$DIR $TARGET/ | tee -a "$TARGET/backup.log"
 done
 
 umount $TARGET
