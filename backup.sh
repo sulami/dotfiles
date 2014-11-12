e9728b05359d41d3dac41e39a28b9ea2c6a3f5c4
Robin Schroer <sulami@peerwire.org>
2014-11-12 10:14:45 +0100

    backup.sh: auto-mount and umount

--- backup.sh
+++ backup.sh
@@ -9,10 +9,14 @@ DIRS=("Documents"
 
 TARGET="/home/sulami/mnt/raspi/"
 
+mount $TARGET
+
 for DIR in ${DIRS[*]}
 do
     echo "$PARENT$DIR -> $TARGET$DIR"
     echo "[$(date --rfc-3339=date)] $PARENT$DIR" >> $TARGET"backup.log"
     rsync -aPq --delete $PARENT$DIR $TARGET | tee -a $TARGET"backup.log"
 done
+
+umount $TARGET
 
