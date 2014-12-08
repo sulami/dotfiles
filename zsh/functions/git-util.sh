e6e2747d7f04e5ba917c7d506463a18ad3e829ab
Robin Schroer <sulami@peerwire.org>
2014-12-08 08:43:05 +0100

    zsh: fix git_add_remote() in several places

--- zsh/functions/git-util.sh
+++ zsh/functions/git-util.sh
@@ -2,10 +2,10 @@
 
 git_add_remote()
 {
-    if [[ ! "$2" =~ "^(http|https|git)://.*" ]]
+    if [[ ! "$2" =~ ^(http|https|git)://.* ]]
     then
-        mkdir -p $2 && git init --bare $2
+        mkdir -p "$2" && git init --bare "$2"
     fi
-    git remote add $1 $2
+    git remote add "$1" "$2"
 }
 
