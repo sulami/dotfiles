ff3edd5133f57cdf798cd72e44be4e875f4ebfc2
Robin Schroer <sulami@peerwire.org>
2014-12-11 21:29:30 +0100

    zsh: add argument count checking to git_add_remote

--- zsh/functions/git-util.sh
+++ zsh/functions/git-util.sh
@@ -2,6 +2,12 @@
 
 git_add_remote()
 {
+    if [ "$#" -ne 2 ]
+    then
+        echo "Usage: $0 <name> <location>" >&2
+        return 1
+    fi
+
     if [[ ! "$2" =~ ^(http|https|git)://.* ]]
     then
         mkdir -p "$2" && git init --bare "$2"
