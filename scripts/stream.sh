f927cbf9e98b4ca5c539fa1d5424fd0f5c7cc8f4
Robin Schroer <sulami@peerwire.org>
2015-02-08 00:20:04 +0100

    stream.sh: add dynamic options

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,29 +1,72 @@
-RES="1280x720"
-FPS="30"
-STREAM_KEY="live_27370333_oiBEe7bfi71fz3wIr27CRXpmszQts5"
+RESOLUTION="1280x720"
+FRAMERATE="30"
+STREAMKEY="live_27370333_oiBEe7bfi71fz3wIr27CRXpmszQts5"
+THREADS="8"
+BITRATE="2048"
+AUDIO="pulse"
+CHANNELS="2"
+VERBOSE=false
+
+while getopts ":r:f:k:t:a:c:v" opt; do
+    case $opt in
+        r)
+            RESOLUTION=$OPTARG
+            ;;
+        f)
+            FRAMERATE=$OPTARG
+            ;;
+        k)
+            STREAMKEY=$OPTARG
+            ;;
+        t)
+            THREADS=$OPTARG
+            ;;
+        b)
+            BITRATE=$OPTARG
+            ;;
+        a)
+            AUDIO=$OPTARG
+            ;;
+        c)
+            CHANNELS=$OPTARG
+            ;;
+        v)
+            VERBOSE=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires and argument" >&2
+            exit 1
+            ;;
+    esac
+done
+
+for c in `seq ${CHANNELS}`; do
+    AUDIOCODE="$AUDIOCODE -f $AUDIO -i default"
+done
 
 ffmpeg \
 -f x11grab \
 -video_size hd1080 \
--framerate $FPS \
+-framerate ${FRAMERATE} \
 -i :0.0+0,0 \
--f pulse \
--i default \
--f pulse \
--i default \
+$AUDIOCODE \
 -vcodec libx264 \
 -preset medium \
--s $RES \
--b:v 2048k \
--minrate 2048k \
--maxrate 2048k \
--bufsize 4096k \
--g 60 \
--filter_complex amix=inputs=2:duration=first:dropout_transition=3 \
+-s ${RESOLUTION} \
+-b:v ${BITRATE}k \
+-minrate ${BITRATE}k \
+-maxrate ${BITRATE}k \
+-bufsize $((2 * ${BITRATE}))k \
+-g $((2 * ${FRAMERATE})) \
+-filter_complex amix=inputs=${CHANNELS}:duration=first:dropout_transition=3 \
 -acodec libmp3lame \
 -ar 44100 \
 -b:a 128k \
--threads 8 \
+-threads ${THREADS} \
 -pix_fmt yuv420p \
--f flv "rtmp://live.justin.tv/app/$STREAM_KEY"
+-f flv rtmp://live.justin.tv/app/${STREAMKEY}
 
