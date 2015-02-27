415671d6681842da4e6e61d2a859d6f083951918
Robin Schroer <sulami@peerwire.org>
2015-02-27 14:22:30 +0100

    zsh: add simplified git init

--- zsh/functions/git.sh
+++ zsh/functions/git.sh
@@ -15,3 +15,11 @@ git_add_remote()
     git remote add "$1" "$2"
 }
 
+git_init()
+{
+    git init
+    PWD=$(pwd | sed 's/\//\n/g' | tail -1)
+    git_add_remote origin ${HOME}/git/${PWD}.git
+    git_add_remote gh https://github.com/sulami/${PWD}
+}
+
