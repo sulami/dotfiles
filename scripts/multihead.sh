791d39c632bc8fb47a9989bc4be62a5d5bd9973b
Robin Schroer <sulami@peerwire.org>
2015-03-16 14:21:22 +0100

    scripts: delete the old multihead.sh

--- scripts/multihead.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Opens two Xephyr windows and connects them via Xinerama
-
-Xephyr +extension RANDR :1 &
-Xephyr +extension RANDR :2 &
-Xdmx +xinerama -xinput local -display :1 -display :2 -ac :3
-
