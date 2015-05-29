937f1582f3ed906b5cb37b931c2b82315f7f11d9
Robin Schroer <sulami@peerwire.org>
2015-05-29 13:51:21 +0200

    mutt/offlineimap: use gpg2 explicitly to use gpg-agent

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -7,7 +7,7 @@ def passwd(acc):
     Get a password from a gpg file, expects something like 'password = "bla"'
     """
     path = '/home/sulami/dotfiles/mutt/mutt-{}.gpg'.format(acc)
-    args = ['gpg', '--use-agent', '-q', '-d', path]
+    args = ['gpg2', '-q', '-d', path]
     try:
         return check_output(args).strip().split('"')[1]
     except CalledProcessError:
