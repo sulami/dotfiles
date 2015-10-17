4d34ee821db88e54af9eea0ca4bb91d3d2913084
Robin Schroer <sulami@peerwire.org>
2015-10-17 17:22:41 +0200

    zsh: add a twitch() function

--- zsh/functions/vlc.sh
+++ zsh/functions/vlc.sh
@@ -3,3 +3,8 @@ ytpl2vlc()
     youtube-dl -g $1 | xargs vlc
 }
 
+twitch()
+{
+    livestreamer --player "vlc --file-caching 2000 --network-caching 2000" --hls-segment-threads 3 twitch.tv/$1 best
+}
+
