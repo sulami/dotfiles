0f3c2954c75ab5e96b2f4edf2e14f56a013199a5
Robin Schroer <sulami@peerwire.org>
2015-10-21 15:41:37 +0200

    zsh: add a twitch vod function for vlc

--- zsh/functions/vlc.sh
+++ zsh/functions/vlc.sh
@@ -8,3 +8,8 @@ twitch()
     livestreamer --player "vlc --file-caching 2000 --network-caching 2000" --hls-segment-threads 3 twitch.tv/$1 best
 }
 
+twitch-vod()
+{
+    livestreamer --player "vlc --file-caching 2000 --network-caching 2000" --hls-segment-threads 3 --player-passthrough=hls $1 best
+}
+
