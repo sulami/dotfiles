6dde059410117f5f881a044d8e0d9f42e897261a
Robin Schroer <sulami@peerwire.org>
2015-10-24 13:07:35 +0200

    zsh: add proper quotes for twitch-vod

--- zsh/functions/vlc.sh
+++ zsh/functions/vlc.sh
@@ -10,6 +10,6 @@ twitch()
 
 twitch-vod()
 {
-    livestreamer --player "vlc --file-caching 2000 --network-caching 2000" --hls-segment-threads 3 --player-passthrough=hls $1 best
+    livestreamer --player "vlc --file-caching 2000 --network-caching 2000" --hls-segment-threads 3 --player-passthrough=hls "$1" best
 }
 
