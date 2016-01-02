6fa9a404e5c4ecfcc35eb78cf0ed17b999bf4733
Robin Schroer <sulami@peerwire.org>
2016-01-02 19:01:41 +0100

    backup.sh: capture stderr in the log as well

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -15,7 +15,7 @@ for DIR in ${DIRS[*]}
 do
     echo "$PARENT/$DIR -> $TARGET/$DIR"
     echo "[$(date --rfc-3339=date)] $PARENT/$DIR" >> "$TARGET/backup.log"
-    rsync -aPq --delete $PARENT/$DIR $TARGET/ | tee -a "$TARGET/backup.log"
+    rsync -aPq --delete $PARENT/$DIR $TARGET/ 2>&1 | tee -a "$TARGET/backup.log"
 done
 
 umount $TARGET
