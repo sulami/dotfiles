2a9ebab27414b87713212c0a3b295d4059ead63b
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-04-08 21:49:12 +0100

    Update offlineimap to use Fastmail

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+from os.path import expanduser
 from subprocess import check_output, CalledProcessError
+
 
 def passwd(acc):
     """
     Get a password from a gpg file, expects something like 'password = "bla"'
     """
-    path = '/home/sulami/dotfiles/mutt/mutt-{}.gpg'.format(acc)
+    path = '{}/dotfiles/mutt/mutt-{}.gpg'.format(expanduser('~'), acc)
     args = ['gpg2', '-q', '-d', path]
     try:
         return check_output(args).strip().split('"')[1]
     except CalledProcessError:
         return 'FAIL'
-
