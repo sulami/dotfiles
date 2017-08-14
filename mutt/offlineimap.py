0f6ef4a92e4a6debac1fe32e7d35cdb9f6f25948
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-08-14 20:02:07 +0100

    Got to use gpg now

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -9,7 +9,7 @@ def passwd(acc):
     Get a password from a gpg file, expects something like 'password = "bla"'
     """
     path = '{}/dotfiles/mutt/mutt-{}.gpg'.format(expanduser('~'), acc)
-    args = ['gpg2', '-q', '-d', path]
+    args = ['gpg', '-q', '-d', path]
     try:
         return check_output(args).strip().split('"')[1]
     except CalledProcessError:
