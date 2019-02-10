5dc5fc1a4e449bcc4f3b044666d60f68eb9f18b9
Robin Schroer <sulami@peerwire.org>
2019-02-10 16:44:12 +0100

    Stop leinigen from bumping versions ahead of time

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -9,8 +9,15 @@
                        [criterium "0.4.4"]]
         :middleware [cider-nrepl.plugin/middleware
                      io.aviso.lein-pretty/inject]
+
         :aliases {"rebl" ["run" "-m" "rebel-readline.main"]
                   "retl" ["trampoline" "run" "-m" "rebel-readline.main"]}
+
+        :release-tasks [["vcs" "assert-committed"]
+                        ["change" "version" "leiningen.release/bump-version" "release"]
+                        ["vcs" "commit"]
+                        ["vcs" "tag"]
+                        ["deploy"]
         :deploy-repositories [["releases" {:url "https://repo.clojars.org"
                                            :creds :gpg}]]
         :signing {:gpg-key "0xA1418106"}}}
