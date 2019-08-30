fc7309b07693cb51d57e46b19d3f4860595c4fa2
Robin Schroer <sulami@peerwire.org>
2019-08-30 15:26:48 +0200

    emacs: tinker on startup performance

--- emacs/.emacs/init.el
+++ emacs/.emacs/init.el
@@ -1,5 +1,18 @@
+;; -*- lexical-binding: t; -*-
+
+;; Startup time optimisations stolen from doom-emacs (including the lexical
+;; binding bit above)
+;; Prevent GC during launch
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+;; We don't need file handlers during launch either
+(defvar sulami--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 ;; Don't use package.el
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil
+      package--init-file-ensured t)
 
 ;; Bootstrap straight
 (defvar bootstrap-version)
@@ -25,4 +38,11 @@
 ;; Eval the actual config
 (require 'org-install)
 (require 'ob-tangle)
-(mapc #'org-babel-load-file (directory-files "/Users/sulami/.emacs" t "\\.org$"))
+(org-babel-load-file "/Users/sulami/.emacs/README.org")
+
+;; Reset file handlers & GC settings after we're done loading
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist sulami--file-name-handler-alist)
+            (setq gc-cons-threshold 16777216
+                           gc-cons-percentage 0.1)))
