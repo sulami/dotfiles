b501a7b058ae3311a1a912d54a69101cc52292b7
Robin Schroer <sulami@peerwire.org>
2019-08-30 21:29:06 +0200

    emacs: a lot more work on the new config

--- emacs/.emacs/init.el
+++ emacs/.emacs/init.el
@@ -38,11 +38,12 @@
 ;; Eval the actual config
 (require 'org-install)
 (require 'ob-tangle)
-(org-babel-load-file "/Users/sulami/.emacs/README.org")
+(defconst sulami/emacs-config-file  "/Users/sulami/.emacs/README.org")
+(org-babel-load-file sulami/emacs-config-file)
 
 ;; Reset file handlers & GC settings after we're done loading
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq file-name-handler-alist sulami--file-name-handler-alist)
             (setq gc-cons-threshold 16777216
-                           gc-cons-percentage 0.1)))
+                  gc-cons-percentage 0.1)))
