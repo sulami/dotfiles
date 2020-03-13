a7593338a97937997034ba208b898d4a447648a4
Robin Schroer <sulami@peerwire.org>
2020-03-13 18:17:48 +0100

    restic: strip newlines off pass output

--- restic/.restic/backup.rb
+++ restic/.restic/backup.rb
@@ -4,10 +4,10 @@ restic_bin = '/usr/local/bin/restic'
 restic_source = '/Users/sulami/Documents /Users/sulami/Pictures --exclude=".DS_Store"'
 retention_policy = '--keep-daily 14 --keep-weekly 4 --keep-monthly 12 --keep-yearly 5'
 
-ENV['B2_ACCOUNT_ID'] = `pass backblaze-b2/account-id`
-ENV['B2_ACCOUNT_KEY'] = `pass backblaze-b2/account-key`
-ENV['RESTIC_REPOSITORY'] = `pass restic/target`
-ENV['RESTIC_PASSWORD'] = `pass restic/password`
+ENV['B2_ACCOUNT_ID'] = `pass backblaze-b2/account-id`.strip
+ENV['B2_ACCOUNT_KEY'] = `pass backblaze-b2/account-key`.strip
+ENV['RESTIC_REPOSITORY'] = `pass restic/target`.strip
+ENV['RESTIC_PASSWORD'] = `pass restic/password`.strip
 
 case ARGV
 when ['backup']
