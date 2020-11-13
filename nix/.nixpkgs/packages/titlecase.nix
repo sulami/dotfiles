7611282de233d3e09502c7832da9612c457775cd
Robin Schroer <git@sulami.xyz>
2020-11-13 16:26:38 +0100

    nix: prettify the titlecase package a bit
    Still needs proper hashes, but I'm too lazy.

--- nix/.nixpkgs/packages/titlecase.nix
+++ nix/.nixpkgs/packages/titlecase.nix
@@ -3,21 +3,29 @@
 
 let appName = "titlecase";
     version = "1.1.0";
-    platform_mapping = {
+    platform = {
       "x86_64-linux" = "x86_64-unknown-linux";
       "x86_64-darwin" = "x86_64-apple-darwin";
       "x86_64-freebsd" = "x86_64-unknown-freebsd";
       "x86_64-netbsd" = "x86_64-unknown-netbsd";
       "x86_64-openbsd" = "x86_64-unknown-openbsd";
-    };
+    }."${stdenv.system}" or (throw "Unsupported platform");
+    sha256 = {
+      # TODO Get the other SHAs.
+      "x86_64-linux" = "0000000000000000000000000000000000000000000000000000";
+      "x86_64-darwin" = "12cs13wsz58k0wijs0gxd8h8dg7cwysjc1cbn347v1z2chjgkxxf";
+      "x86_64-freebsd" = "0000000000000000000000000000000000000000000000000000";
+      "x86_64-netbsd" = "0000000000000000000000000000000000000000000000000000";
+      "x86_64-openbsd" = "0000000000000000000000000000000000000000000000000000";
+    }."${stdenv.system}" or (throw "Unsupported platform");
 
 in stdenv.mkDerivation {
   pname = appName;
   version = version;
 
   src = fetchurl {
-    url = "https://releases.wezm.net/${appName}/${version}/${appName}-${version}-${platform_mapping.${builtins.currentSystem}}.tar.gz";
-    sha256 = "12cs13wsz58k0wijs0gxd8h8dg7cwysjc1cbn347v1z2chjgkxxf";
+    url = "https://releases.wezm.net/${appName}/${version}/${appName}-${version}-${platform}.tar.gz";
+    sha256 = "${sha256}";
   };
 
   setSourceRoot = "sourceRoot=`pwd`";
