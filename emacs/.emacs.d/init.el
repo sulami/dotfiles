344d0befe172336aecdef8b461e699bb7c42e515
Robin Schroer <git@sulami.xyz>
2021-03-13 12:13:26 +0100

    emacs: Tweak the startup sequence
    Moving the benchmarking into init.el, and manually capturing the time
    at startup should lead to more accurate measurements.
    
    Also waiting a few seconds after startup before we reset GC settings,
    which means anything lazy-loaded by use-package after startup will
    still benefit from the more relaxed settings.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,6 +6,8 @@
 ;; also minimise startup speed. This currently starts up on my machine
 ;; in about 0.3s.
 
+(defconst emacs-start-time (current-time))
+
 ;; Startup time optimisations stolen from doom-emacs (including the lexical
 ;; binding bit above)
 ;; Prevent GC during launch
@@ -63,10 +65,20 @@
   (defconst sulami/emacs-config-file "~/.emacs/README.org")
   (org-babel-load-file sulami/emacs-config-file))
 
-;; Reset file handlers & GC settings after we're done loading
+;; Reset file handlers & GC settings after we're done loading.
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq file-name-handler-alist sulami--file-name-handler-alist)
-            (setq gc-cons-threshold 16777216
-                  gc-cons-percentage 0.1)
-            (setq debug-on-error nil)))
+            (run-with-timer 5
+                            nil
+                            (lambda ()
+                              (setq file-name-handler-alist sulami--file-name-handler-alist)
+                              (setq gc-cons-threshold 16777216
+                                    gc-cons-percentage 0.1)
+                              (setq debug-on-error nil)))))
+
+;; Startup done, print some statistics.
+(let ((pkg-count (length (hash-table-keys straight--success-cache)))
+      (startup-time (float-time (time-subtract (current-time) emacs-start-time))))
+  (message (format "Startup complete, loaded %d packages in %.2fs"
+                   pkg-count
+                   startup-time)))
