644f46a1b9b0fb8b61342164d8dda46faf767240
Robin Schroer <git@sulami.xyz>
2024-01-25 09:45:28 +0900

    nvim: Add some more config tweaks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,6 +15,19 @@ vim.api.nvim_exec([[
   set textwidth=80
 ]], false)
 
+-- Setup smartcase for searching.
+vim.api.nvim_exec([[
+  set ignorecase
+  set smartcase
+]], false)
+
+-- Tabs are two spaces.
+vim.api.nvim_exec([[
+  set expandtab
+  set tabstop=2
+  set shiftwidth=2
+]], false)
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -28,16 +41,37 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{ "kylechui/nvim-surround",
-		event = "VeryLazy",
-      		config = function()
-		require("nvim-surround").setup({
-		  -- Configuration here, or leave empty to use defaults
-		})
-	      end
-      },
-    "b0o/mapx.nvim",
-    }
-)
+local lazy = require("lazy")
+local plugins = {
+  "b0o/mapx.nvim",
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+		    -- Configuration here, or leave empty to use defaults
+		  })
+	  end
+  },
+}
 
+if vim.g.vscode then
+  -- VS Code config.
+  lazy.setup(plugins)
+else
+  -- Plain neovim config, load more plugins.
+  table.insert(plugins, {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  })
+  table.insert(plugins, {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    }
+  })
+  lazy.setup(plugins)
+end
