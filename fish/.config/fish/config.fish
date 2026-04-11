4b534465a27d6cfc30a6dc8a602d6fac0aa9ce25
Robin Schroer <git@sulami.xyz>
2026-04-11 20:47:35 +0900

    mise: Use mise for cargo binstalls as well

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
   set -gx EDITOR nvim
   set -gx VISUAL $EDITOR
 
-  fish_add_path ~/.cargo/bin
   fish_add_path ~/.local/bin
   fish_add_path /opt/homebrew/bin
   fish_add_path /opt/homebrew/sbin
