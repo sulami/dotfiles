2e07194ef54107d214070a4527cfc729c7f51317
Robin Schroer <git@sulami.xyz>
2026-04-11 19:36:25 +0900

    General cleanup, switch to mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,55 +1,37 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -gx EDITOR nvim
-    set -gx VISUAL $EDITOR
+  # Commands to run in interactive sessions can go here
+  set -gx EDITOR nvim
+  set -gx VISUAL $EDITOR
 
-    fish_add_path ~/.cargo/bin
-    fish_add_path ~/.asdf/shims
-    fish_add_path /opt/homebrew/bin
-    fish_add_path /opt/homebrew/sbin
+  fish_add_path ~/.cargo/bin
+  fish_add_path ~/.local/bin
+  fish_add_path /opt/homebrew/bin
+  fish_add_path /opt/homebrew/sbin
 
-    alias ls='eza -F'
-    alias ll='ls -l'
-    alias la='ll -a'
-    alias g='git'
-    alias dc='docker-compose'
-    alias tf='terraform'
+  alias ls='eza -F'
+  alias ll='ls -l'
+  alias la='ll -a'
+  alias g='git'
+  alias dc='docker-compose'
+  alias tf='terraform'
+  alias uuid="uuidgen | tr '[:upper:]' '[:lower:]' | pbcopy"
 
-    function psg
-      ps aux | grep $argv
-    end
+  function psg
+    ps aux | grep $argv
+  end
 
-    function mcd
-      mkdir -p $argv && cd $argv
-    end
+  function mcd
+    mkdir -p $argv && cd $argv
+  end
 
-    function spinout
-      if test -z "$argv[1]"
-        echo "Error: No branch name supplied"
-        return 1
-      end
-      if test -n "$(git status --porcelain)"
-        echo "Error: Worktree is dirty"
-        return 1
-      end
-      set CURRENT_BRANCH (git rev-parse --abbrev-ref HEAD)
-      git checkout -b $argv[1]
-      git checkout $CURRENT_BRANCH
-      git reset --hard origin/$CURRENT_BRANCH
-      git checkout $argv[1]
-    end
+  # Mise
+  mise activate fish | source
 
-    # Zoxide
-    eval "$(zoxide init fish)"
+  # Zoxide
+  eval "$(zoxide init fish)"
 
-    # Flox
-    eval "$(flox activate --dir ~)"
-
-    # GPG Agent
-    if test -e "$(gpgconf --list-dirs agent-ssh-socket)" -a -n "$(pgrep gpg-agent)"
-        set -gx SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
-    else
-        eval $(gpg-agent --daemon)
-        set -gx SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
-    end
+  # SSH through Bitwarden
+  if test -S "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
+    set -gx SSH_AUTH_SOCK "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
+  end
 end
